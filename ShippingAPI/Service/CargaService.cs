836a63e5abe4da20478b42a51e052357a261bdb1
Guilherme Silva <90401412+codelheira@users.noreply.github.com>
2021-09-23 03:08:44 -0300

    Adicionado lazy load para o tipo veiculo na entidade Carga

--- ShippingAPI/Service/CargaService.cs
+++ ShippingAPI/Service/CargaService.cs
@@ -11,14 +11,16 @@ namespace ShippingAPI.Service
         public CargaService(ShippingContext context) : base(context)
         {
         }
-        public override Carga FindById(int id) => ShippingContext.Set<Carga>()
+        public override Carga FindById(int id) => ShippingContext.Carga
                 .Include(x => x.Veiculo)
                 .Include(x => x.Status)
+                .Include(x => x.Veiculo.TipoVeiculo)
                 .Where(x => x.Id == id).FirstOrDefault();
 
         public override IEnumerable<Carga> FindAll() => ShippingContext.Carga
                 .Include(x => x.Veiculo)
                 .Include(x => x.Status)
+                .Include(x => x.Veiculo.TipoVeiculo)
                 .ToList();
     }
 }
